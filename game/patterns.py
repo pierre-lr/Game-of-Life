908be6c2c3630571e0374a052b2924aca007680d
Pierre La Rocca <pierre.la-rocca@etu.utc.fr>
2024-11-13 21:55:51 +0100

    wip: adding TOML data

--- game/patterns.py
+++ game/patterns.py
@@ -4,3 +4,10 @@ from dataclasses import dataclass
 class Pattern:
     name: str
     alive_cells: set[tuple[int,int]] #each tuple of the set represents the coordinates of an alive cell
+
+    @classmethod
+    def from_toml(cls,name,toml_data):
+        return cls(
+            name,
+            alive_cells={tuple(cell) for cell in toml_data["alive_cells"]},
+        )
